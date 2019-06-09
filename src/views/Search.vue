7cd344547fbf8ff31187e5e647b577afc3a98b40
k.szyszko14@gmail.com <Krzysiek14>
2019-06-09 12:52:34 +0200

    eslint fix

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -22,30 +22,29 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import debounce from 'lodash.debounce';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+
 const API = 'https://images-api.nasa.gov/search';
 export default {
-    name: 'Search',
-    data() {
-      return {
-          searchValue: '',
-          results: [],
-      }
-    },
-    methods: {
-        handleInput: debounce(function() {
-            {
-          axios.get(`${API}?q=${this.searchValue}&media_type=image`)
-              .then((response) => {
-                  this.results = response.data.collection.items;
-              })
-              .catch((error) => {
-                  console.log(error);
-              })
-            }
-        }, 500)
-    },
+  name: 'Search',
+  data() {
+    return {
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    handleInput: debounce(function () {
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
 };
 </script>
 <style lang="scss" scoped>
