89f7c4b2d7f3e8479eef83a0426d974842a37ae1
k.szyszko14@gmail.com <Krzysiek14>
2019-06-13 00:24:49 +0200

    search animation + modal + loader

--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -18,17 +18,34 @@
 <style lang="scss" scoped>
     .claimWrapper {
         display: flex;
+        text-align: center;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        color: white;
     }
 
     .claim {
         font-size: 50px;
         margin: 0;
+
+        @media (min-width: 768px) {
+            font-size: 80px;
+        }
+        @media (min-width: 1024px) {
+            font-size: 96px;
+        }
     }
     .subclaim {
         margin: 20px 0 0 0;
         font-size: 13px;
+        font-weight: 400;
+
+        @media (min-width: 768px) {
+            font-size: 17px;
+            line-height: 1;
+            width: 500px;
+            letter-spacing: 1px;
+        }
     }
 </style>
